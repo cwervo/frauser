e061eeb6f337a509cfd3622484ff4deb0945e85f
Andrés Cuervo <andrescuervor@gmail.com>
2024-08-05 11:45:18 -0400

    Basic graph printing, better description

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Frauser
 
 - "Folk browser" -> "Frowser" -> "Frauser" ("frau" + "zer")
+- Tcl CLI HTML parser thing (for now, to become a browser)
+
+## Try it out
+
+```
+./brauser.tcl
+```
--- brauser.tcl
+++ brauser.tcl
@@ -16,15 +16,26 @@ puts $html
 # set htmlContent [read [open $htmlFile]]
 
 # Step 3: Parse the HTML content into a DOM graph
-set dom [dom parse -html $htmlContent]
+set dom [dom parse -html $html]
 
 # Step 4: Optionally, print the DOM structure
-puts [$dom asXML]
+set dom_xml [$dom asXML]
+puts $dom_xml
+
+# New function to print DOM as ASCII graph
+proc print_dom_ascii {node {prefix ""}} {
+    puts "${prefix}[string tolower [$node nodeName]]"
+    foreach child [$node childNodes] {
+        if {[$child nodeType] == "ELEMENT_NODE"} {
+            print_dom_ascii $child "${prefix}  |--"
+        }
+    }
+}
+
+# Print the DOM as an ASCII graph
+puts "\nDOM Structure as ASCII graph:"
+print_dom_ascii [$dom documentElement]
+
 # 2. Print it out as an ASCII diagram (valid Dot format probably, to start)
-
-
-
-
-
 
 http::cleanup $token
